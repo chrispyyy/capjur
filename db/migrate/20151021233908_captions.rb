fbbd07e6e0f9ffd86ba98f481c234b5fb05e3988
Chris Brooks <chris1_b@hotmail.com>
2015-10-22 00:08:50 +0000

    fixed tables

--- db/migrate/20151021233908_captions.rb
+++ db/migrate/20151021233908_captions.rb
@@ -1,11 +1,11 @@
 class Captions < ActiveRecord::Migration
   def change
     create_table :captions do |t|
+      t.references :image
+      t.references :user
       t.string :text
       t.integer :total_votes
       t.timestamps null: false
-      t.reference :image
-      t.reference :user
     end
   end
 end
